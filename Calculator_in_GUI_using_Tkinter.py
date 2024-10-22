aadbbc4d5566dfeea7ed83563e206fc31fce3f7d
waghmareaniket <124170359+waghmareaniket@users.noreply.github.com>
2024-10-23 00:14:03 +0530

    .
    .

---
+++ Calculator_in_GUI_using_Tkinter.py
@@ -0,0 +1,122 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print('helo')
+helo
+
+======================== RESTART: C:/Users/Admin/Calculatore.py ========================
+Traceback (most recent call last):
+  File "C:/Users/Admin/Calculatore.py", line 47, in <module>
+    self.Calculator=Calculator(root)
+NameError: name 'self' is not defined
+
+======================== RESTART: C:/Users/Admin/Calculatore.py ========================
+Traceback (most recent call last):
+  File "C:/Users/Admin/Calculatore.py", line 47, in <module>
+    self.Calculator=Calculator(root)
+NameError: name 'self' is not defined
+
+======================== RESTART: C:/Users/Admin/Calculatore.py ========================
+Traceback (most recent call last):
+  File "C:/Users/Admin/Calculatore.py", line 47, in <module>
+    self.Calculator=Calculator(root)
+NameError: name 'self' is not defined
+
+======================== RESTART: C:/Users/Admin/Calculatore.py ========================
+Traceback (most recent call last):
+  File "C:/Users/Admin/Calculatore.py", line 47, in <module>
+    self.Calculator=Calculator(root)
+NameError: name 'self' is not defined
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Program Files\Python313\Lib\tkinter\__init__.py", line 2068, in __call__
+    return self.func(*args)
+           ~~~~~~~~~^^^^^^^
+  File "C:/Users/Admin/Calculatore.py", line 43, in solve
+    result=eval(self.entry_value)
+  File "<string>", line 1
+    5x9
+    ^
+SyntaxError: invalid decimal literal
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Program Files\Python313\Lib\tkinter\__init__.py", line 2068, in __call__
+    return self.func(*args)
+           ~~~~~~~~~^^^^^^^
+  File "C:/Users/Admin/Calculatore.py", line 43, in solve
+    result=eval(self.entry_value)
+  File "<string>", line 1
+    5x9
+    ^
+SyntaxError: invalid decimal literal
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Program Files\Python313\Lib\tkinter\__init__.py", line 2068, in __call__
+    return self.func(*args)
+           ~~~~~~~~~^^^^^^^
+  File "C:/Users/Admin/Calculatore.py", line 43, in solve
+    result=eval(self.entry_value)
+  File "<string>", line 1
+    5x9
+    ^
+SyntaxError: invalid decimal literal
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Program Files\Python313\Lib\tkinter\__init__.py", line 2068, in __call__
+    return self.func(*args)
+           ~~~~~~~~~^^^^^^^
+  File "C:/Users/Admin/Calculatore.py", line 43, in solve
+    result=eval(self.entry_value)
+  File "<string>", line 1
+    5x9
+    ^
+SyntaxError: invalid decimal literal
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Program Files\Python313\Lib\tkinter\__init__.py", line 2068, in __call__
+    return self.func(*args)
+           ~~~~~~~~~^^^^^^^
+  File "C:/Users/Admin/Calculatore.py", line 43, in solve
+    result=eval(self.entry_value)
+  File "<string>", line 1
+    5x9
+    ^
+SyntaxError: invalid decimal literal
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Program Files\Python313\Lib\tkinter\__init__.py", line 2068, in __call__
+    return self.func(*args)
+           ~~~~~~~~~^^^^^^^
+  File "C:/Users/Admin/Calculatore.py", line 43, in solve
+    result=eval(self.entry_value)
+  File "<string>", line 1
+    5x9
+    ^
+SyntaxError: invalid decimal literal
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Program Files\Python313\Lib\tkinter\__init__.py", line 2068, in __call__
+    return self.func(*args)
+           ~~~~~~~~~^^^^^^^
+  File "C:/Users/Admin/Calculatore.py", line 43, in solve
+    result=eval(self.entry_value)
+  File "<string>", line 1
+    5x9
+    ^
+SyntaxError: invalid decimal literal
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Program Files\Python313\Lib\tkinter\__init__.py", line 2068, in __call__
+    return self.func(*args)
+           ~~~~~~~~~^^^^^^^
+  File "C:/Users/Admin/Calculatore.py", line 43, in solve
+    result=eval(self.entry_value)
+  File "<string>", line 1
+    5x9
+    ^
+SyntaxError: invalid decimal literal
+
+======================== RESTART: C:/Users/Admin/Calculatore.py ========================
+Traceback (most recent call last):
+  File "C:/Users/Admin/Calculatore.py", line 47, in <module>
+    self.Calculator=Calculator(root)
+NameError: name 'self' is not defined
